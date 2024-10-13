4f3789990498e147b1b7559ea9df5e8b86751c2c
contrerascs <60404702+contrerascs@users.noreply.github.com>
2024-10-12 21:44:50 -0600

    Actualización

--- WS_Selenium.py
+++ WS_Selenium.py
@@ -2,10 +2,13 @@
 from selenium import webdriver
 #import para inicializar nuestro WebDriver
 from selenium.webdriver.chrome.service import Service
+import time
 
-path = 'Sam Contreras/Documents/Programacion/Python/ChromeDriver'
+path = 'D:/Sam Contreras/Documents/Programacion/Python/ChromeDriver/chromedriver.exe'
 servicio = Service(executable_path=path)
 driver = webdriver.Chrome(service=servicio)
 web = 'https://en.wikipedia.org/wiki/1982_FIFA_World_Cup'
 
 driver.get(web)
+matches = driver.find_elements(by='xpath', value='//td[@align="right"]/..')
+time.sleep(10)
