d0891c890969d2f14084bd10f099976de886c56e
contrerascs <60404702+contrerascs@users.noreply.github.com>
2024-10-15 23:48:24 -0600

    Actualización

--- WS_Selenium.py
+++ WS_Selenium.py
@@ -9,7 +9,7 @@ from bs4 import BeautifulSoup
 import time
 
 years = [1930, 1934, 1938, 1950, 1954, 1958, 1962, 1966, 1970, 1974,
-         1978, 1982, 1986, 1990, 1994, 1998, 2002, 2010, 2014,
+         1978, 1982, 1986, 1990, 1994, 1998, 2002, 2006, 2010, 2014,
          2018, 2022]
 
 path = 'D:/Sam Contreras/Documents/Programacion/Python/ChromeDriver/chromedriver.exe'
@@ -21,7 +21,6 @@ def obtener_data_faltante(year):
 
     driver.get(web)
     matches = driver.find_elements(By.XPATH, value='//tr[@style="font-size:90%"]')
-
     home = []
     score = []
     away = []
@@ -55,4 +54,4 @@ def obtener_data_faltante(year):
 fifa = [obtener_data_faltante(year) for year in years]
 driver.quit()
 df_fifa = pd.concat(fifa,ignore_index=True)
-df_fifa.to_csv('Datasets/FIFA_HISTORY_COMPLETE.csv', index=False)    
+df_fifa.to_csv('Datasets/Fifa_WorldCup_Matches_Include_2022.csv', index=False)    
